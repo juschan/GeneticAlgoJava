b4445cb72fa4a813ffc35aba7b2a5135811580a7
Justin Chan <justinchanuk@gmail.com>
2017-12-22 20:32:22 +0800

    Added more methods to GeneticAlgorithm.java

--- chapter2/GeneticAlgorithm.java
+++ chapter2/GeneticAlgorithm.java
@@ -15,7 +15,47 @@ public class GeneticAlgorithm {
     }
 
     public Population initPopulation(int chromosomeLength) {
-        Population population = newPopulation(this.populationSize, chromosomeLength);
+        Population population = new Population(this.populationSize, chromosomeLength);
         return population;
     }
+
+
+    public double calcFitness(Individual individual) {
+        //track number of correct genes
+        int correctGenes = 0;
+
+        for (int geneIndex = 0; geneIndex < individual.getChromosomeLength(); geneIndex++) {
+            if (individual.getGene(geneIndex)==1) {
+                correctGenes+=1;
+            }
+        }
+
+        //calculate fitness
+        double fitness = (double) correctGenes/ individual.getChromosomeLength();
+
+        //store fitness
+        individual.setFitness(fitness);
+
+        return fitness;
+    }
+
+    public void evalPopulation(Population population) {
+        double populationFitness = 0;
+        for (Individual individual : population.getIndividuals()) {
+            populationFitness += calcFitness(individual);
+        }
+
+        population.setPopulationFitness(populationFitness);
+    }
+ 
+    
+    public boolean isTerminationConditionMet(Population population) {
+        for (Individual individual: population.getIndividuals()) {
+            if(individual.getFitness() ==1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
