0dbf5e01fd5b35c59e009ac5a75ddd6fdaf87a80
Justin Chan <justinchanuk@gmail.com>
2017-12-22 19:39:36 +0800

    Added AllOnesGA.java. Starting point for AllOnesGA app. Barebones

---
+++ chapter2/AllOnesGA.java
@@ -0,0 +1,9 @@
+package chapter2;
+
+//bootstrap class - starting point with main method for AllOnesGA app
+public class AllOnesGA {
+
+    public static void main(String[] args) {
+        GeneticAlgorithm ga = new GeneticAlgorithm(100, 0.01, 0.95, 0);
+    }
+}
