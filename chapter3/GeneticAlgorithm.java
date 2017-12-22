27f9e968402151e827b89ca7c07e19d28d449ae5
Justin Chan <justinchanuk@gmail.com>
2017-12-22 21:53:16 +0800

    Created chapter3 folder with 3 java files chapter2 with modifications

---
+++ chapter3/GeneticAlgorithm.java
@@ -0,0 +1,49 @@
+package chapter3;
+
+public class GeneticAlgorithm {
+    private int populationSize;
+    private double mutationRate;
+    private double crossoverRate;
+    private int elitismCount;
+
+    //constructor
+    public GeneticAlgorithm(int populationSize, double mutationRate, double crossoverRate, int elitismCount) {
+        this.populationSize = populationSize;
+        this.mutationRate = mutationRate;
+        this.crossoverRate = crossoverRate;
+        this.elitismCount = elitismCount;
+    }
+
+    public Population initPopulation(int chromosomeLength) {
+        Population population = new Population(this.populationSize, chromosomeLength);
+        return population;
+    }
+
+
+
+
+    public Population mutatePopulation(Population population) {
+        Population newPopulation = new Population(this.populationSize);
+
+        for (int populationIndex =0; populationIndex <population.size(); populationIndex++) {
+            Individual individual = population.getFittest(populationIndex);
+
+            for (int geneIndex=0; geneIndex < individual.getChromosomeLength(); geneIndex++) {
+                if(populationIndex>=this.elitismCount) {
+                    if(this.mutationRate > Math.random()) {
+                        int newGene=1;
+                        if (individual.getGene(geneIndex)==1) {
+                            newGene=0;
+                        }
+                        //mutate gene
+                        individual.setGene(geneIndex,newGene);
+                    }
+                }
+            }
+            newPopulation.setIndividual(populationIndex, individual);
+        }
+        return newPopulation;
+    }
+
+
+}
