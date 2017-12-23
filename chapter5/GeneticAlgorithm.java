efc46315804a31c6aed6bb704f13b861964531ee
Justin Chan <justinchanuk@gmail.com>
2017-12-23 14:25:00 +0800

    Commit initial files for chapter5 class scheduling

---
+++ chapter5/GeneticAlgorithm.java
@@ -0,0 +1,78 @@
+package chapter4;
+
+import java.util.Arrays;
+
+public class GeneticAlgorithm {
+    private int populationSize;
+    private double mutationRate;
+    private double crossoverRate;
+    private int elitismCount;
+
+    protected int tournamentSize;
+
+    //constructor
+    public GeneticAlgorithm(int populationSize, double mutationRate, double crossoverRate, int elitismCount, int tournamentSize) {
+        this.populationSize = populationSize;
+        this.mutationRate = mutationRate;
+        this.crossoverRate = crossoverRate;
+        this.elitismCount = elitismCount;
+        this.tournamentSize = tournamentSize;
+    }
+
+    public boolean isTerminationConditionMet(int generationsCount, int nextGenerations) {
+        return (generationsCount > nextGenerations);
+    }
+
+    public Individual selectParent(Population population) {
+        Population tournament = new Population(this.tournamentSize);
+        
+        //add random individuals to the tournament
+        population.shuffle();
+        for(int i=0; i<this.tournamentSize; i++) {
+            Individual tournamentIndividual=population.getIndividual(i);
+            tournament.setIndividual(i, tournamentIndividual);
+        }
+        return tournament.getFittest(0);
+    }
+
+
+
+    public Population crossoverPopulation(Population population) {
+        //create new population
+        Population newPopulation = new Population(population.size());
+
+        for (int populationIndex =0; populationIndex<population.size(); populationIndex++) {
+            Individual parent1 = population.getFittest(populationIndex);
+        
+            //apply crossover to this individual?
+            if(this.crossoverRate > Math.random() && populationIndex > this.elitismCount) {
+                //initialise offspring
+                Individual offspring = new Individual(parent1.getChromosomeLength());
+
+                //find second parent
+                Individual parent2 = selectParent(population);
+
+                //loop over genome
+                for (int geneIndex=0; geneIndex<parent1.getChromosomeLength(); geneIndex++) {
+                    //use half of each parent's genes. Uniform crossover
+                    if(0.5 > Math.random()) {
+                        offspring.setGene(geneIndex, parent1.getGene(geneIndex));
+                    } else {
+                        offspring.setGene(geneIndex, parent2.getGene(geneIndex));
+                    }
+                }
+
+                //add offspring to new population
+                newPopulation.setIndividual(populationIndex, offspring);
+            
+            } else {
+                //add individual to new population without applying crossover
+                newPopulation.setIndividual(populationIndex, parent1);
+            }
+        }
+        return newPopulation;
+
+    }
+
+
+}
---
+++ chapter5/Individual.java
@@ -0,0 +1,66 @@
+package chapter4;
+
+
+//Individual class represents single candidate solution. 
+public class Individual {
+    private int[] chromosome;
+    private double fitness =-1;
+
+    //constructor
+    public Individual(int[] chromosome) {
+        this.chromosome = chromosome;
+    }
+
+    public Individual(int chromosomeLength) {
+        int[] individual;
+        individual = new int[chromosomeLength];
+
+        for(int gene=0; gene < chromosomeLength; gene++) {
+            individual[gene] = gene;
+        }
+        this.chromosome = individual;
+    }
+
+
+    public int[] getChromosome() {
+        return this.chromosome;
+    }
+
+    public int getChromosomeLength() {
+        return this.chromosome.length;
+    }
+
+    public void setGene(int offset, int gene) {
+        this.chromosome[offset] = gene;
+    }
+
+    public int getGene(int offset) {
+        return this.chromosome[offset];
+    }
+
+    public void setFitness(double fitness) {
+        this.fitness = fitness;
+    }
+
+    public double getFitness() {
+        return this.fitness;
+    }
+
+    public String toString() {
+        String output = "";
+        for (int gene=0; gene < this.chromosome.length; gene++) {
+            output +=this.chromosome[gene];
+        }
+        return output;
+    }
+
+    public boolean containsGene(int gene) {
+        for (int i=0; i<this.chromosome.length; i++) {
+            if(this.chromosome[i] == gene) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
