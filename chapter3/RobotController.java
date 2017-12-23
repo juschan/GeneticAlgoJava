c98494f88276283f2f7a026ed64ef653977faae4
Justin Chan <justinchanuk@gmail.com>
2017-12-23 12:45:01 +0800

    Correction to maze setup and argument to ga constructor in RobotController

--- chapter3/RobotController.java
+++ chapter3/RobotController.java
@@ -19,14 +19,14 @@ public class RobotController {
             {1,0,1,1,1,0,1,3,1},
             {1,0,0,1,3,3,3,3,1},
             {3,3,3,1,3,1,1,0,1},
-            {3,1,3,3,3,1,1,0,0}.
+            {3,1,3,3,3,1,1,0,0},
             {3,3,1,1,1,1,0,1,1},
             {1,3,0,1,3,3,3,3,3},
             {0,3,1,1,3,1,0,1,3},
             {1,3,3,3,3,1,1,1,4}
          });
 
-         GeneticAlgorithm ga = new GeneticAlgorithm(200, 0,05, 0.9, 2, 10);
+         GeneticAlgorithm ga = new GeneticAlgorithm(200, 0.05, 0.9, 2, 10);
          Population population = ga.initPopulation(128);
 
          int generation=1;
